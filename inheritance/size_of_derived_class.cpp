baa6e39b37f2e23f1f9f8fc7027a23e15cb2c76b
Lavin-tom <lavintom007@gmail.com>
2023-01-11 19:24:59 +0530

    added more codes

---
+++ inheritance/size_of_derived_class.cpp
@@ -0,0 +1,20 @@
+//inheritance
+//size of inheritance
+
+#include<iostream>
+using namespace std;
+class base
+{
+	int x,y;
+};
+class derived : public base
+{
+	//int m,n;
+
+};
+int main()
+{
+	//size of class is not based on private or public, still it create memory for private data too 
+	cout<<"size of : "<<sizeof(base)<<endl;				//8
+	cout<<"size of : "<<sizeof(derived)<<endl;			//16
+}
