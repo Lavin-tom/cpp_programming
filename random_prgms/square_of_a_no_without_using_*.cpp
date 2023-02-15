7fd00ec873900288bc9671a946c0235d8b3381f0
Lavin-tom <lavintom007@gmail.com>
2023-02-15 20:48:05 +0530

    added more codes

---
+++ random_prgms/square_of_a_no_without_using_*.cpp
@@ -0,0 +1,14 @@
+//wap to find the square of a number without using *
+
+#include <iostream>
+using namespace std;
+int main(){
+	int n,sum=0;
+	cout<<"enter number"<<endl;
+	cin>>n;
+	
+	for(int i=0;i<n;i++)
+		sum=sum+n;
+	
+	cout<<sum<<endl;
+}
